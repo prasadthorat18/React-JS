d1aeb91f0816f5ea9e36e871788cc67c383a663a
prasadthorat18 <prasadthorat552@gmail.com>
2026-09-18 17:40:11 +0530

    created cards using plain css

---
+++ 04-Card-Project/src/App.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+import { Bookmark } from "lucide-react";
+
+const App = () => {
+  return (
+    <div className="parent">
+      <div className="card">
+        <div>
+          <div className="top">
+            <img
+              src="https://imgs.search.brave.com/-QlnopsS72o8z4m37MBvznkKqNpLr4D87VV3CRwrBjU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzk0LzU0LzA3/LzM2MF9GXzI5NDU0/MDcyMl9WSXdTV3ly/VTZxNzgxU2tIV1Ns/elVEWGpmb3VSbGo0/Ny5qcGc"
+              alt=""
+            />
+            <button>
+              Save <Bookmark size={10} />
+            </button>
+          </div>
+
+          <div className="center">
+            <h3>
+              Amazon <span>5 days ago</span>
+            </h3>
+            <h2>Senior UI/UX Designer</h2>
+            <div className="tag">
+              <h4>Part-Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div>
+            <h4>120$/hr</h4>
+            <h6>Mumbai, India</h6>
+          </div>
+          <button>Apply now</button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div>
+          <div className="top">
+            <img
+              src="https://imgs.search.brave.com/-QlnopsS72o8z4m37MBvznkKqNpLr4D87VV3CRwrBjU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzk0LzU0LzA3/LzM2MF9GXzI5NDU0/MDcyMl9WSXdTV3ly/VTZxNzgxU2tIV1Ns/elVEWGpmb3VSbGo0/Ny5qcGc"
+              alt=""
+            />
+            <button>
+              Save <Bookmark size={10} />
+            </button>
+          </div>
+
+          <div className="center">
+            <h3>
+              Amazon <span>5 days ago</span>
+            </h3>
+            <h2>Senior UI/UX Designer</h2>
+            <div className="tag">
+              <h4>Part-Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div>
+            <h4>120$/hr</h4>
+            <h6>Mumbai, India</h6>
+          </div>
+          <button>Apply now</button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div>
+          <div className="top">
+            <img
+              src="https://imgs.search.brave.com/-QlnopsS72o8z4m37MBvznkKqNpLr4D87VV3CRwrBjU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzk0LzU0LzA3/LzM2MF9GXzI5NDU0/MDcyMl9WSXdTV3ly/VTZxNzgxU2tIV1Ns/elVEWGpmb3VSbGo0/Ny5qcGc"
+              alt=""
+            />
+            <button>
+              Save <Bookmark size={10} />
+            </button>
+          </div>
+
+          <div className="center">
+            <h3>
+              Amazon <span>5 days ago</span>
+            </h3>
+            <h2>Senior UI/UX Designer</h2>
+            <div className="tag">
+              <h4>Part-Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div>
+            <h4>120$/hr</h4>
+            <h6>Mumbai, India</h6>
+          </div>
+          <button>Apply now</button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div>
+          <div className="top">
+            <img
+              src="https://imgs.search.brave.com/-QlnopsS72o8z4m37MBvznkKqNpLr4D87VV3CRwrBjU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzk0LzU0LzA3/LzM2MF9GXzI5NDU0/MDcyMl9WSXdTV3ly/VTZxNzgxU2tIV1Ns/elVEWGpmb3VSbGo0/Ny5qcGc"
+              alt=""
+            />
+            <button>
+              Save <Bookmark size={10} />
+            </button>
+          </div>
+
+          <div className="center">
+            <h3>
+              Amazon <span>5 days ago</span>
+            </h3>
+            <h2>Senior UI/UX Designer</h2>
+            <div className="tag">
+              <h4>Part-Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div>
+            <h4>120$/hr</h4>
+            <h6>Mumbai, India</h6>
+          </div>
+          <button>Apply now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
